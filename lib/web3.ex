a7388443a41b696bb7b2203084b6c24f60431704
Wu Minzhe <wuminzhe@gmail.com>
2017-06-21 11:25:47 +0800

    remove filter

--- lib/web3.ex
+++ lib/web3.ex
@@ -1,16 +1,3 @@
 defmodule Web3 do
-  use Application
-
-  def start(_type, _args) do
-
-    import Supervisor.Spec
-    children = [
-      worker(Task, [Web3.Filter, :watch, []])
-    ]
-
-    opts = [strategy: :one_for_one, name: Web3.Filter.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+  
 end
-
-
